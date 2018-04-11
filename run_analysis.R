3e8da1141200a4b80fb2f9f5f0b823b6e9f5219c
blewis49 <jblewis49@me.com>
2018-04-10 21:41:03 -0400

    Completed project

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,12 @@
-## R Programming Assignment 4
+## R Programming Assignment 4 - A Tidy Dataset
 ## Author: William Lewis
 ## Date: 8 April 2018
-#############################
+## -------------------------------------------
 library(dplyr)
 
 # Step 1: Merge the training and test data into one dataset
-# Read in the x, y, subject data for both test and training with read.table()
+      # Read in the x, y, subject data for both test and training with read.table()
+
       X_train <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/train/X_train.txt", header = F)
       y_train <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/train/y_train.txt")
       subject_train <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/train/subject_train.txt")
@@ -14,47 +15,45 @@ library(dplyr)
       y_test <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/test/y_test.txt")
       X_test <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/test/X_test.txt", header = F)
 
-# combine the test data by column in the order of subject, y, and x.  Do the same for training data
-# combine the test and training data by rows that results in one large dataset
-# each row represent an individual subject
+      # combine the test data by column in the order of subject, y, and x.  Do the same for training data
+      # combine the test and training data by rows that results in one large dataset
+      # each row represents an individual subject
+      
       merged_training <- cbind(subject_train, y_train, X_train)
       merged_test <- cbind(subject_test, y_test, X_test)
       merged_data <- rbind(merged_test, merged_training)
       colnames(merged_data) <- c("Subjects", "Activities", as.character(features[,2]))
 
-# Step 2: Extract (select) only the columns (measurements) that contain a mean or std measurement
-      # merged_data[,grep("mean()", names(merged_data))] # code to extract column names with mean()
-      # merged_data[,grep("std()", names(merged_data))] # code to extract column names with std()
-      # merged_data[,1:2] # code to extract the first two columns
-# extract only measurements with mean() and std() in the names
+# Step 2: Extract (select) only the columns (activities) that contain a mean or std measurement
+      # Use the escape character (\) on the special characters
+      
       merged_data_extract <- cbind(merged_data[,1:2], merged_data[,grep("mean\\(\\)", names(merged_data))],
                              merged_data[,grep("std\\(\\)", names(merged_data))])
-      # this grep command does not catch names that end in "mean()"
       
 # Step 3: Use descriptive activity names to name the activities in the dataset
+      
       activity_labels <- read.table("~/Documents/Programming/R_Programming/UCI HAR Dataset/activity_labels.txt", header = F)
       names(activity_labels) <- c("Index", "Activity") # change the variable names
-# using subsetting, change the number values representing activities into words for activities
+
+      # using subsetting, change the number values representing activities into words for activities
       merged_data_extract$Activities <- activity_labels$Activity[merged_data_extract$Activities]
       
-
 # Step 4: Appropriately label the dataset with descriptive variable names
-# use colnames and sub to make variable name changes
-      colnames(merged_data_extract) <- sub("tB", "time.B", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("tG", "time.G", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("fB", "freq.B", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("fG", "freq.G", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-mean\\(\\)\\-X", ".mean.X", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-mean\\(\\)\\-Y", ".mean.Y", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-mean\\(\\)\\-Z", ".mean.Z", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-mean\\(\\)", ".mean", colnames(merged_data_extract))
+      # use the colnames() and sub() functions to make descriptive variable name changes
       
-      colnames(merged_data_extract) <- sub("\\-std\\(\\)\\-X", ".std.X", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-std\\(\\)\\-Y", ".std.Y", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-std\\(\\)\\-Z", ".std.Z", colnames(merged_data_extract))
-      colnames(merged_data_extract) <- sub("\\-std\\(\\)", ".std", colnames(merged_data_extract))
+      colnames(merged_data_extract) <- sub("^t", "time", colnames(merged_data_extract))
+      colnames(merged_data_extract) <- sub("^f", "freq", colnames(merged_data_extract))
+      colnames(merged_data_extract) <- gsub("\\-", ".", colnames(merged_data_extract))
+      colnames(merged_data_extract) <- gsub("\\(\\)", "", colnames(merged_data_extract))
 
-# need to create a loop to achieve the result above.  Too much repeated code
+# Step 5: Create an independent tidy dataset with the avg of each variable for each activity and subject
       
-
-
+      tidy_dataset <- merged_data_extract %>% 
+                  group_by(Subjects, Activities) %>% 
+                  summarize_all(mean) %>% 
+                  ungroup(merged_data_extract)
+      
+      
+      
+      
+      
